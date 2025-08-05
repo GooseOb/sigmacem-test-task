72b034adfbfafcba10ab6dabc2333751fdfa4bc2
GooseOb <gooose.ob@gmail.com>
2025-08-06 00:46:18 +0200

    add gh workflow for deploying to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { Header } from "./components/Header";
 import type { Product } from "./types";
 import { Search } from "./components/Search";
